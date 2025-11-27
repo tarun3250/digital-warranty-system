f50e6ca760da099bb2b5c7cf27b1146726e960fa
tarun3250 <tarun03250@gmail.com>
2025-11-27 21:32:14 +0530

    Fix: Added Render DB environment variables and Sequelize config update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,19 +8,27 @@
 
 // module.exports = sequelize;
 
+
+
+
+
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
-    port: process.env.DB_PORT || 5432,
-    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
 module.exports = sequelize;
-
